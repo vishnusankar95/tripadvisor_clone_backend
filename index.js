97fbf15841d4f6dc89478251411016e30b9afa67
Vishnu <82999551+vishnusankar95@users.noreply.github.com>
2021-12-03 22:06:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
 
+const connect = require('./configs/db');
+require('dotenv').config()
+
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded())
